531e311b0ee99feca30dcde57eaeec8d8f621e87
Kriket <kriket267@gmail.com>
2018-09-22 10:33:26 +0300

    eslint fix

--- src/components/modals/sign-up.component.jsx
+++ src/components/modals/sign-up.component.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 export default class SignUp extends React.Component {
 
-    render() {
-        return (
-            <div id="sign-up">
-                <h3>Log In</h3>
-                <form action="">
-                    <input class="reg_form" type="text" placeholder="E-mail"/>
-                    <input class="reg_form" type="password" placeholder="Password"/>
-                    <input class="reg_form" type="password" placeholder="Password repeat"/>
-                    <div class="btn">Log In</div>
-                </form> 
-            </div>   
-        )
-    }
+	render() {
+		return (
+			<div id="sign-up">
+				<h3>Log In</h3>
+				<form action="">
+					<input className="reg_form" type="text" placeholder="E-mail" />
+					<input className="reg_form" type="password" placeholder="Password" />
+					<input className="reg_form" type="password" placeholder="Password repeat" />
+					<div className="btn">Log In</div>
+				</form>
+			</div>
+		);
+	}
+
 }
